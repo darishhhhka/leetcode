231fc07aedd9724ff01a7a78e490cdbd802e2b86
tarusovadarina6 <tarusovadarina6@gmail.com>
2025-11-16 21:19:00 +0300

    Calculator with Method Chaining

---
+++ Calculator.js
@@ -0,0 +1,40 @@
+class Calculator {
+  constructor(value) {
+    this.result = value;
+  }
+
+  add(value) {
+    this.result += value;
+    return this;
+  }
+
+  subtract(value) {
+    this.result -= value;
+    return this;
+  }
+
+  multiply(value) {
+    this.result *= value;
+    return this;
+  }
+
+  divide(value) {
+    if (!value) {
+      throw new Error("Division by zero is not allowed");
+    }
+    this.result = this.result / value;
+    return this;
+  }
+
+  power(value) {
+    this.result = this.result ** value;
+    return this;
+  }
+
+  getResult() {
+    return this.result;
+  }
+}
+
+const num = new Calculator(20).divide(0).getResult();
+console.log(num);
